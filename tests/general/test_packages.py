3cddd516b33e456faf6095decf21bdca1b910065
Bryan Paget <bryanpaget@pm.me>
2025-07-19 20:05:23 -0400

    chore: reformat py file

--- tests/general/test_packages.py
+++ tests/general/test_packages.py
@@ -10,7 +10,7 @@ It's a basic test aiming to prove that the package is working properly.
 The goal is to detect import errors that can be caused by incompatibilities between packages for example:
 
 - #1012: issue importing `sympy`
-- #966: isssue importing `pyarrow`
+- #966: issue importing `pyarrow`
 
 This module checks dynmamically, through the `CondaPackageHelper`, only the specified packages i.e. packages requested by `conda install` in the `Dockerfiles`.
 This means that it does not check dependencies. This choice is a tradeoff to cover the main requirements while achieving reasonable test duration.
