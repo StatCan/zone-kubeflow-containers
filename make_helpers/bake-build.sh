3ce1a52cc849047d12d422f6b0d8d80783a4c5d9
Mathis Marcotte <84033116+mathis-marcotte@users.noreply.github.com>
2025-09-02 12:17:51 +0000

    feat: add docker-bake to build process (#135)
    Adds docker bake as the solution for building our images.

---
+++ make_helpers/bake-build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Builds the desired image using bake, setting the needed parameters to override the bake file values
+# Inputs: 
+#   BASE_IMAGE (The base parent image for the image to build)
+#   REPO (The location where our images live)
+#   TAGS (Any desired tags on the final image, expects a space seperated list of values)
+#   IMAGE_NAME (The name of the bake target to build)
+#   DARGS (Any additional parameters to set to the bake command)
+
+ARGS=()
+# if there is a base image given, set that as the value for the BASE_IMAGE arg in the docker bake
+[[ -n "${BASE_IMAGE}" ]] && ARGS+=("--set ${IMAGE_NAME}.args.BASE_IMAGE=${BASE_IMAGE}")
+
+TAG_LIST=()
+if [[ -n "${TAGS}" ]]; then
+    TAG_LIST=($TAGS)
+    # for each tag given, prefix the tag with the bake value to override the tags from the bake file
+    TAG_LIST=( "${TAG_LIST[@]/#/--set ${IMAGE_NAME}.tags=${REPO}/${IMAGE_NAME}:}" )
+    
+    # concats the list of tag overrides to the bake parameters list
+    ARGS+=( "${TAG_LIST[@]}" )
+fi
+
+docker buildx bake ${IMAGE_NAME} ${DARGS} ${ARGS[@]}
