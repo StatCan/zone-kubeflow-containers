f79001d3bbceba3dc60195754bc5252b92f8b544
Mathis Marcotte <mathis.marcotte@canada.ca>
2025-12-05 14:31:32 +0000

    added certs

---
+++ images/mid/certs/getCerts.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# Script used to generate the .crt files for the given host
+HOST=stc-ndm-new-qa-wc2.statcan.gc.ca
+
+openssl s_client -showcerts -connect "${HOST}":443 < /dev/null |
+   awk '/BEGIN CERTIFICATE/,/END CERTIFICATE/{ if(/BEGIN CERTIFICATE/){a++}; out="cert"a".pem"; print >out}'
+for cert in *.pem; do 
+        newname=$(openssl x509 -noout -subject -in $cert | sed -nE 's/.*CN ?= ?(.*)/\1/; s/[ ,.*]/_/g; s/__/_/g; s/_-_/-/; s/^_//g;p' | tr '[:upper:]' '[:lower:]').crt
+        echo "${newname}"; mv "${cert}" "${newname}" 
+done
