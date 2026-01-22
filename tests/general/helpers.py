0c123bc22cdc2fe4aa8c0e0faafb4ae175ff7d4b
Bryan Paget <8212170+bryanpaget@users.noreply.github.com>
2026-01-22 14:40:05 -0500

    chore: restore comment

--- tests/general/helpers.py
+++ tests/general/helpers.py
@@ -99,6 +99,7 @@ class CondaPackageHelper:
     @staticmethod
     def _packages_from_json(env_export):
         """Extract packages and versions from the lines returned by the list of specifications"""
+        # dependencies = filter(lambda x:  isinstance(x, str), json.loads(env_export).get("dependencies"))
         # Handle cases where stderr messages are mixed with JSON output
         # Find the start and end of the actual JSON to handle stderr messages
         start = env_export.find("{")
